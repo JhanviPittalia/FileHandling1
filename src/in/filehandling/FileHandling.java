024829a70e4ab36ad22010118094c10ddb8b30d8
JhanviPittalia <Jhanvi@technous.in>
2023-12-07 16:24:07 +0530

    first

--- src/in/filehandling/FileHandling.java
+++ src/in/filehandling/FileHandling.java
@@ -13,6 +13,7 @@ public class FileHandling {
             BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
             System.out.println("1)OutputStream");
             System.out.println("1)OutputStream");
+            //https://github.com/RushitaSudani/javacollectionRepo1.git
             System.out.println("2)InputStream");
             System.out.println("3)BufferedInputStream");
             System.out.println("4)BufferedOutputStream");
